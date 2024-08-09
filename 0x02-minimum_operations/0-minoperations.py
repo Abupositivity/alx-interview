8e23f453c9b02927ffaa6e32a46e0b8c6386fd60
abupositivity <abupositivity@gmail.com>
2024-08-09 03:05:13 +0000

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Minimum Operation's Module"""
+
+def minOperations(n):
+    """
+    Gets the fewest no of ops needed to end up with exactly
+    n 'x' characters.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n /= factor
+        factor += 1
+
+    return operations
